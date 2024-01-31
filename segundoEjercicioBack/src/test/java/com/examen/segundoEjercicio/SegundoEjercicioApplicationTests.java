d9b4e2aeedbdb1767e5e3ab1f147c117ee65fdab
LLESCA <leandro.lesca@ayi.group>
2024-01-31 20:46:39 -0300

    primer cambio

---
+++ segundoEjercicioBack/src/test/java/com/examen/segundoEjercicio/SegundoEjercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.segundoEjercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoEjercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
